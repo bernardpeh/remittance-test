3e589ba418aeef1f625f2090b6111a1be89495a6
bernard peh <bpeh@sitecritic.net>
2017-09-28 00:09:28 +1000

    update code

--- Remittance.sol
+++ Remittance.sol
@@ -2,22 +2,23 @@ pragma solidity 0.4.17;
 
 contract Remittance {
   
-  address owner;
-  address withdrawer;
-  bytes32 password1;
-  bytes32 password2;
-  uint dateDue;
+  address public owner;
+  address public withdrawer;
+  bytes32 private password1;
+  bytes32 private password2;
+  uint public dueBlocks;
   
-  function Remittance(bytes32 _password1, bytes32 _password2, uint _dateDue, address _withdrawer) public {
+  function Remittance(bytes32 _password1, bytes32 _password2, uint _due, address _withdrawer) public {
       owner = msg.sender;
       withdrawer = _withdrawer;
-      dateDue = block.number + _dateDue;
+      dueBlocks = block.number + _due;
       password1 = keccak256(_password1);
       password2 = keccak256(_password2);
   }
   
   function fund() public payable returns (bool) {
       require(msg.sender == owner);
+      require(msg.value > 0);
       return true;
   }
   
@@ -25,8 +26,21 @@ contract Remittance {
       return this.balance;
   }
   
+  function getCurrentBlock() public view returns (uint) {
+    return block.number;    
+  }
+  
   function withdraw(bytes32 _password1, bytes32 _password2) public returns (bool) {
-      kill(withdrawer);
+      require(msg.sender == withdrawer || msg.sender == owner);
+      // if still within time limit
+      if (block.number >= dueBlocks) {
+        if (password1 == keccak256(_password1) && password2 == keccak256(_password2)) {
+          kill(withdrawer);
+        }
+      }
+      else {
+          kill(owner);
+      }
       return true;
   }
   
